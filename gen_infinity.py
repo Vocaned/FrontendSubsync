fe535c0a6e8875d0fbe237a0276592841e40c66d
Fam0r <fam0r@mailbox.org>
2022-08-26 17:18:48 +0300

    Use pathlib for everything instead of os

--- gen_infinity.py
+++ gen_infinity.py
@@ -1,6 +1,6 @@
 import json
+from pathlib import Path
 import pyminizip
-import os
 import toml
 
 from utils import ensure_output, cache_subs
@@ -20,21 +20,25 @@ def main():
         entries.append({
             'favorite': False,
             'username': '-',
-            'name': sub_info[sub]['name'],
-            'id': sub_info[sub]['id'],
-            'iconUrl': sub_info[sub]['iconUrl']
+            'name': sub_info[sub.lower()]['name'],
+            'id': sub_info[sub.lower()]['id'],
+            'iconUrl': sub_info[sub.lower()]['iconUrl']
         })
 
-    with open('output/cache/anonymous_subscribed_subreddits.json', 'w', encoding='utf-8') as f:
+    temp_json = Path('output/cache/anonymous_subscribed_subreddits.json')
+
+    with open(temp_json, 'w', encoding='utf-8') as f:
         json.dump(entries, f)
 
-    pyminizip.compress('output/cache/anonymous_subscribed_subreddits.json', '5.2.0/database/', 'output/infinity_backup.zip', '123321', 0)
+    # Infinity backup zips are password protected with the password `123321`
+    # https://github.com/Docile-Alligator/Infinity-For-Reddit/blob/ddfc478e0e9f95b8630a16dd3a179bb4acfca10a/app/src/main/java/ml/docilealligator/infinityforreddit/asynctasks/RestoreSettings.java#L88
+    password = '123321'
+    pyminizip.compress(str(temp_json), '5.2.0/database/', 'output/infinity_backup.zip', password, 0)
 
-    os.remove('output/cache/anonymous_subscribed_subreddits.json')
-
-    print('Subscriptions written to output/infinity_backup.zip. Import the file in Advanced > Restore Settings in Infinity')
+    temp_json.unlink()
 
 if __name__ == '__main__':
     ensure_output()
     cache_subs()
     main()
+    print('Subscriptions written to output/infinity_backup.zip. Import the file in Advanced > Restore Settings in Infinity')
